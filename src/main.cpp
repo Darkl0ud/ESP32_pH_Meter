bd306cfbea9c862f16bfde4085477788a3cccdb1
Darkl0ud <wjbrown831989@gmail.com>
2023-11-05 22:51:21 -0500

    Created main.h. pH Calibration calculations complete.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,7 @@
 /* Made by Darkloud for the DIY-MORE PH-4502C pH controller board and used specifically
  on the ESP32-S3 DevKitC */
 
-#include <Arduino.h>
-
-#define RGB_BRIGHTNESS 1 //  Brightness setting for RGB LEDs
-#define PH_READ_PIN 8 // GPIO 8, assigned to pH probe analog input
-
-float fpHArray[10];
-int j;
-unsigned long currentMillis = millis();
-static unsigned long fLastMillis;  // Last millisec trig
-static unsigned long fLastSecond; // Last second trig
+#include <main.h>
 
 void setup() {
   neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0);
@@ -43,43 +34,89 @@ void SetDeviceState(int state) {  // Sets various indicators depending upon syst
 }
 
 void RunpH() {
-  currentMillis = millis();
+  lCurrentMillis = millis();
 
-  if (currentMillis - fLastMillis >= 100) {
-    fLastMillis = currentMillis;  // Save the current time
-    fpHArray[j] = analogRead(PH_READ_PIN) / 4095.0 * 3.3 * 4.24; // 4.24 = 14/3.3
+  if (lCurrentMillis - lLastMillis >= 100) { // Once 100ms have passed...
+    lLastMillis = lCurrentMillis;  // Save the current time
+
+    rawAnalog = analogRead(PH_READ_PIN) / 4095.0; // Read from pH probe. Gives an analog 0-1 value.
+    float b;
+    float pH;
+
+    if (rawAnalog >= fpH14Cal && rawAnalog <= fpH10Cal)  // pH 14 - 10
+    {
+      b = 14 - ((14.0 - 10.0) / (fpH14Cal - fpH10Cal)) * fpH14Cal;  // Calculate Y Intercept based off of pH Calibration slope
+      pH = ((14.0 - 10.0) / (fpH14Cal - fpH10Cal)) * rawAnalog + b;
+    }
+    
+    if (rawAnalog >= fpH10Cal && rawAnalog <= fpH7Cal)  // pH 10 - 7
+    {
+      b = 10 - ((10.0 - 7.0) / (fpH10Cal - fpH7Cal)) * fpH10Cal;  // Calculate Y Intercept based off of pH Calibration slope
+      pH = ((10.0 - 7.0) / (fpH10Cal - fpH7Cal)) * rawAnalog + b;
+    }
+
+    if (rawAnalog >= fpH7Cal && rawAnalog <= fpH4Cal)  // pH 7 - 4
+    {
+      b = 7 - ((7.0 - 4.0) / (fpH7Cal - fpH4Cal)) * fpH7Cal;  // Calculate Y Intercept based off of pH Calibration slope
+      pH = ((7.0 - 4.0) / (fpH7Cal - fpH4Cal)) * rawAnalog + b;
+    }
+
+    if (rawAnalog >= fpH4Cal && rawAnalog <= fpH0Cal)  // pH 4 - 0
+    {
+      b = 4 - ((4.0 - 0.0) / (fpH4Cal - fpH0Cal)) * fpH4Cal;  // Calculate Y Intercept based off of pH Calibration slope
+      pH = ((4.0 - 0.0) / (fpH4Cal - fpH0Cal)) * rawAnalog + b;
+    }
+
+    fpHAnalogArray[j] = pH;
     j++;
   }
    
-  if (currentMillis - fLastSecond >= 1000) {
-    fLastSecond = currentMillis;  // Save the current time
+  if (lCurrentMillis - lLastSecond >= 1000) {  // Once 1000ms have passed...
+    lLastSecond = lCurrentMillis;  // Save the current time
     j = 0; // Resets j to 0
-    int maxIndex = 0; // Resets maxIndex to 0
-    int minIndex = 0; // Resets minIndex to 0
+    char maxIndex = 0; // Resets maxIndex to 0
+    char minIndex = 0; // Resets minIndex to 0
 
     for (int i = 0; i < 10; i++) {  // Check each reading that we put in the array if our max or min number is higher or lower
-      if (fpHArray[i] > fpHArray[maxIndex]) {
+      if (fpHAnalogArray[i] > fpHAnalogArray[maxIndex]) {
         maxIndex = i;
       }
-      if (fpHArray[i] < fpHArray[minIndex]) {
+      if (fpHAnalogArray[i] < fpHAnalogArray[minIndex]) {
         minIndex = i;
       }
     }
 
-    fpHArray[maxIndex] = 0; // Set the highest value in the pH array to 0 so it is not counted in average.
-    fpHArray[minIndex] = 0; // Set the lowest value in the pH array to 0 so it is not counted in average.
-    float sum = 0;  // Initializes sum variable to 0 for use with determining average pH over the given 10 readings
-    int count = 0;  // Initializes count variable to 0, which holds how many pH readings above 0 we have
+    fpHAnalogArray[maxIndex] = 0; // Set the highest value in the pH array to 0 so it is not counted in average.
+    fpHAnalogArray[minIndex] = 0; // Set the lowest value in the pH array to 0 so it is not counted in average.
+    float fSum = 0;  // Initializes sum variable to 0 for use with determining average pH over the given 10 readings
+    unsigned char cCount = 0;  // Initializes count variable to 0, which holds how many pH readings above 0 we have. Char to save memory
 
     for (int i = 0; i < 10; i++) {  // Check each value in pH array. If not 0, add it to sum and increase count.
-      if (fpHArray[i] != 0) {
-        sum += fpHArray[i];
-        count++;
+      if (fpHAnalogArray[i] != 0) {
+        fSum += fpHAnalogArray[i];
+        cCount++;
       }
     }
-    
-    float fpHAverage = sum/count; // Calculate average of pH readings over 1 second
+    float fpHAverage = fSum/cCount; // Calculate average of pH readings over 1 second
+    Serial.println(fpHAverage);
   }  
+}
+
+void CalibratepH() {
+  Serial.println("Please rinse off probe and place in pH 4 solution, wait until reading is stable, then press button to lock in calibration. ");
+  // Wait for button press.
+  rawAnalog = analogRead(PH_READ_PIN) / 4095.0;
+  fpH4Cal = rawAnalog;
+
+    Serial.println("Please rinse off probe and place in pH 7 solution, wait until reading is stable, then press button to lock in calibration. ");
+  // Wait for button press.
+  rawAnalog = analogRead(PH_READ_PIN) / 4095.0;
+  fpH7Cal = rawAnalog;
+
+    Serial.println("Please rinse off probe and place in pH 10 solution, wait until reading is stable, then press button to lock in calibration. ");
+  // Wait for button press.
+  rawAnalog = analogRead(PH_READ_PIN) / 4095.0;
+  fpH10Cal = rawAnalog;
 }
 
 void loop() {
