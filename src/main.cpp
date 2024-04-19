fe553f8d4fe88f0cc675939989c52aa5563a8419
Darkl0ud <wjbrown831989@gmail.com>
2024-04-19 13:47:25 -0400

    Stripped to pH logic
    Stripped code down to pH logic only. Preparation for screen handling rewrite.

--- src/main.cpp
+++ src/main.cpp
@@ -1,154 +1,44 @@
-/* Made by Darkloud using DIY-MORE PH-4502C pH controller board and used specifically
- on the ESP32-S3 DevKitC */
-
 #include <main.h>
 
 void IRAM_ATTR buttonUpPressed() {
   if (digitalRead(UP_BUTTON_PIN) == 1)  {
-    bUpPressed = true;
+    upPressed = true;
   }
   else  {
-    bUpPressed = false;
+    upPressed = false;
   }
 }
 
 void IRAM_ATTR buttonDownPressed() {
   if (digitalRead(DOWN_BUTTON_PIN) == 1)  {
-    bDownPressed = true;
+    downPressed = true;
   }
   else  {
-    bDownPressed = false;
+    downPressed = false;
   }
 }
 
 void IRAM_ATTR buttonLeftPressed() {
   if (digitalRead(LEFT_BUTTON_PIN) == 1)  {
-    bLeftPressed = true;
+    leftPressed = true;
   }
   else  {
-    bLeftPressed = false;
+    leftPressed = false;
   }
 }
 
 void IRAM_ATTR buttonRightPressed() {
   if (digitalRead(RIGHT_BUTTON_PIN) == 1)  {
-    bRightPressed = true;
+    rightPressed = true;
   }
   else  {
-    bRightPressed = false;
+    rightPressed = false;
   }
 }
 
-void setDeviceState(char state) {  // Sets various indicators depending upon system status.
-
-  switch (state) {
-
-  case 0: // Normal State
-    neopixelWrite(RGB_BUILTIN,0,RGB_BRIGHTNESS,0);  // Set Status RGB Green
-    cDevState = 0;
-    break;
-  case 1: // Fault State
-    neopixelWrite(RGB_BUILTIN,RGB_BRIGHTNESS,0,0);  // Set Status RGB Red
-    cDevState = 1;
-    break;
-  case 2: // Notification State
-    neopixelWrite(RGB_BUILTIN,0,0,RGB_BRIGHTNESS);  // Set Status RGB Blue
-    cDevState = 2;
-    break;
-  default:
-    break;
-  }
-}
-
-void uiHandler(){   // Handles UI IO
-  switch (cSelectedMenuTab) {
-  case 0:
-    dUI.DisplayUpdate(0,fpHAverage,fpHTarget,0, bpHTimerEnded); // Update display with pH parameters.
-
-    if (bUpPressed)
-    {
-      cSelectedMenuTab = 2;
-      bUpPressed = false;
-    }
-
-    else if (bDownPressed)
-    {
-      cSelectedMenuTab = 1;
-      bDownPressed = false;
-    }
-    break;
-  
-  case 1:
-    dUI.DisplayUpdate(1,fpHAverage,fpHTarget,0, bpHTimerEnded); // Update display with EC parameters.
-
-    if (bUpPressed)
-    {
-      cSelectedMenuTab = 0;
-      bUpPressed = false;
-    }
-    
-    else if (bDownPressed)
-    {
-      cSelectedMenuTab = 2;
-      bDownPressed = false;
-    }
-    break;
-
-  case 2:
-    /* When Set tab is active... */
-    if (bMenuTabActive) // If menu is active...
-    {
-      dUI.DisplayUpdate(2,fpHAverage,fpHTarget,1, bpHTimerEnded); // Update display with Setting parameters.
-
-      if (bLeftPressed)
-      {
-        bMenuTabActive = false;
-        bLeftPressed = false;
-      }
-      
-      if (bUpPressed)
-      {
-        fpHTarget = fpHTarget + 0.1;
-        bUpPressed = false;
-      }
-    
-      else if (bDownPressed)
-      {
-        fpHTarget = fpHTarget - 0.1;
-        bDownPressed = false;
-      }
-    }
-    
-    else  // If menu is not active
-    {
-      dUI.DisplayUpdate(2,fpHAverage,fpHTarget,0, bpHTimerEnded); // Update display with Setting parameters.
-
-      if (bUpPressed)
-      {
-        cSelectedMenuTab = 1;
-        bUpPressed = false;
-      }
-    
-      else if (bDownPressed)
-      {
-        cSelectedMenuTab = 0;
-        bDownPressed = false;
-      }
-
-      else if (bRightPressed)
-      {
-        bMenuTabActive = true;
-        bRightPressed = false;
-      }
-    }
-  default:
-    break;
-  }
-}
 
 void setup()  {
   Serial.begin(115200);
-  setDeviceState(0);  // Set device state to 0, 'running'
   pinMode(PH_READ_PIN, INPUT);
   pinMode(UP_BUTTON_PIN, INPUT_PULLDOWN);
   pinMode(DOWN_BUTTON_PIN, INPUT_PULLDOWN);
@@ -158,123 +48,90 @@ void setup()  {
   attachInterrupt(DOWN_BUTTON_PIN, buttonDownPressed, CHANGE);
   attachInterrupt(LEFT_BUTTON_PIN, buttonLeftPressed, CHANGE);
   attachInterrupt(RIGHT_BUTTON_PIN, buttonRightPressed, CHANGE);
-  Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN); // Initialize Wire with custom SDA and SCL pins
-  dUI.InitDisplay();
-  dUI.DisplayUpdate(0,fpHAverage,fpHTarget,0, bpHTimerEnded); // Initial Display Update.
 }
 
 void calculateAveragepH() {
-    if (lCurrentMillis - lLastpHMillis >= 100)  { // Once 100ms have passed...
-    lLastpHMillis = lCurrentMillis;  // Save the current time.
+    if (currentMillis - lastpHMillis >= 100)  { // Once 100ms have passed...
+    lastpHMillis = currentMillis;  // Save the current time.
     rawAnalog = analogRead(PH_READ_PIN) / 4095.0; // Read from pH probe. Gives an analog 0-1 value.
     float b;
     float pH;
 
-    if (rawAnalog >= fpH14Cal && rawAnalog <= fpH10Cal) {  // pH 14 - 10
-      b = 14 - ((14.0 - 10.0) / (fpH14Cal - fpH10Cal)) * fpH14Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-      pH = ((14.0 - 10.0) / (fpH14Cal - fpH10Cal)) * rawAnalog + b;
+    if (rawAnalog >= pH14Cal && rawAnalog <= pH10Cal) {  // pH 14 - 10
+      b = 14 - ((14.0 - 10.0) / (pH14Cal - pH10Cal)) * pH14Cal;  // Calculate Y Intercept based off of pH Calibration slope.
+      pH = ((14.0 - 10.0) / (pH14Cal - pH10Cal)) * rawAnalog + b;
     }
     
-    if (rawAnalog >= fpH10Cal && rawAnalog <= fpH7Cal)  {  // pH 10 - 7
-      b = 10 - ((10.0 - 7.0) / (fpH10Cal - fpH7Cal)) * fpH10Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-      pH = ((10.0 - 7.0) / (fpH10Cal - fpH7Cal)) * rawAnalog + b;
+    if (rawAnalog >= pH10Cal && rawAnalog <= pH7Cal)  {  // pH 10 - 7
+      b = 10 - ((10.0 - 7.0) / (pH10Cal - pH7Cal)) * pH10Cal;  // Calculate Y Intercept based off of pH Calibration slope.
+      pH = ((10.0 - 7.0) / (pH10Cal - pH7Cal)) * rawAnalog + b;
     }
 
-    if (rawAnalog >= fpH7Cal && rawAnalog <= fpH4Cal) {  // pH 7 - 4
-      b = 7 - ((7.0 - 4.0) / (fpH7Cal - fpH4Cal)) * fpH7Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-      pH = ((7.0 - 4.0) / (fpH7Cal - fpH4Cal)) * rawAnalog + b;
+    if (rawAnalog >= pH7Cal && rawAnalog <= pH4Cal) {  // pH 7 - 4
+      b = 7 - ((7.0 - 4.0) / (pH7Cal - pH4Cal)) * pH7Cal;  // Calculate Y Intercept based off of pH Calibration slope.
+      pH = ((7.0 - 4.0) / (pH7Cal - pH4Cal)) * rawAnalog + b;
     }
 
-    if (rawAnalog >= fpH4Cal && rawAnalog <= fpH0Cal) {  // pH 4 - 0
-      b = 4 - ((4.0 - 0.0) / (fpH4Cal - fpH0Cal)) * fpH4Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-      pH = ((4.0 - 0.0) / (fpH4Cal - fpH0Cal)) * rawAnalog + b;
+    if (rawAnalog >= pH4Cal && rawAnalog <= pH0Cal) {  // pH 4 - 0
+      b = 4 - ((4.0 - 0.0) / (pH4Cal - pH0Cal)) * pH4Cal;  // Calculate Y Intercept based off of pH Calibration slope.
+      pH = ((4.0 - 0.0) / (pH4Cal - pH0Cal)) * rawAnalog + b;
     }
-    fpHAnalogArray[j] = pH;
+    pHAnalogArray[j] = pH;
     j++;
   }
    
-  if (lCurrentMillis - lLastSecond >= 1000) {  // Once 1000ms have passed...
-    lLastSecond = lCurrentMillis;  // Save the current time.
+  if (currentMillis - lastSecond >= 1000) {  // Once 1000ms have passed...
+    lastSecond = currentMillis;  // Save the current time.
     j = 0; // Resets j to 0.
     char maxIndex = 0; // Resets maxIndex to 0.
     char minIndex = 0; // Resets minIndex to 0.
 
     for (int i = 0; i < 10; i++) {  // Check each reading that we put in the array if our max or min number is higher or lower.
-      if (fpHAnalogArray[i] > fpHAnalogArray[maxIndex]) {
+      if (pHAnalogArray[i] > pHAnalogArray[maxIndex]) {
         maxIndex = i;
       }
-      if (fpHAnalogArray[i] < fpHAnalogArray[minIndex]) {
+      if (pHAnalogArray[i] < pHAnalogArray[minIndex]) {
         minIndex = i;
       }
     }
 
-    fpHAnalogArray[maxIndex] = 0; // Set the highest value in the pH array to 0 so it is not counted in average.
-    fpHAnalogArray[minIndex] = 0; // Set the lowest value in the pH array to 0 so it is not counted in average.
-    float fSum = 0;  // Initializes sum variable to 0 for use with determining average pH over the given 10 readings.
-    unsigned char cCount = 0;  // Initializes count variable to 0, which holds how many pH readings above 0 we have. Using Char to save memory.
+    pHAnalogArray[maxIndex] = 0; // Set the highest value in the pH array to 0 so it is not counted in average.
+    pHAnalogArray[minIndex] = 0; // Set the lowest value in the pH array to 0 so it is not counted in average.
+    float Sum = 0;  // Initializes sum variable to 0 for use with determining average pH over the given 10 readings.
+    unsigned char countsOverZero = 0;  // Initializes count variable to 0, which holds how many pH readings above 0 we have. Using Char to save memory.
 
     for (int i = 0; i < 10; i++) {  // Check each value in pH array. If not 0, add it to sum and increase count.
-      if (fpHAnalogArray[i] != 0) {
-        fSum += fpHAnalogArray[i];
-        cCount++;
+      if (pHAnalogArray[i] != 0) {
+        Sum += pHAnalogArray[i];
+        countsOverZero++;
       }
     }
-    fpHAverage = fSum/cCount; // Calculate average of pH readings over 1 second
+    pHAverage = Sum/countsOverZero; // Calculate average of pH readings over 1 second
+    Serial.println(pHAverage);
   }
 }
 
 void loop() {
-  lCurrentMillis = millis();
+  currentMillis = millis();
   calculateAveragepH();
-
-  if (fpHTarget != 0) // If we have a pH Target set....
-  {
-    if (!bWaitForMix) // If we are not waiting for mixture to become homogenous...
-    {
-      if (fpHAverage <= (fpHTarget - 0.2))  //If pH drifts from Target pH - 0.2...
-      {
-        bpHUnderRange = true;
-        bpHOverRange = false;
-      }
   
-      else if (fpHAverage >= (fpHTarget + 0.2))  //If pH drifts from Target pH + 0.2...
-      {
-        bpHUnderRange = false;
-        bpHOverRange = true;
-      }
-      
-      else  // If pH is within range...
-      {
-        bpHUnderRange = false;
-        bpHOverRange = false;
-      }
-
-      if (bpHUnderRange || bpHOverRange)  // If pH is out of range...
-      {
-        if (!bpHTimerStarted) // If we don't have a timer started already, then start a timer. If we do, then see if timer has expired.
-        {
-          lTimerStartMillis = lCurrentMillis;
-          bpHTimerStarted = true;
-        }
-        else  // Timer already started
-        {
-          if (lCurrentMillis >= lTimerStartMillis + 5000) // Check if timer is expired...
-          {
-            // Timer expired. Do the things.
-            bpHTimerEnded = true;
-          }
-        }
-      }
-      else  // If pH is no longer out of range...
-      {
-        bpHTimerStarted = false;
-        bpHTimerEnded = false;
-      }   
-    }
-
-    else  // If we are waiting for mixture to become homogenous
+  if (pHTarget != 0) // If we have a pH Target set....
+  {
+    
+    if (pHAverage <= (pHTarget - 0.2))  //If pH drifts from Target pH - 0.2...
     {
+      pHUnderRange = true;
+      pHOverRange = false;
+    }
+    else if (pHAverage >= (pHTarget + 0.2))  //If pH drifts from Target pH + 0.2...
+    {
+      pHUnderRange = false;
+      pHOverRange = true;
+    }
+    else  // If pH is within range...
+    {
+      pHUnderRange = false;
+      pHOverRange = false;
     }
   }
-  uiHandler();
 }
--- src/main.h
+++ src/main.h
@@ -2,42 +2,32 @@
  on the ESP32-S3 DevKitC */
 
 #include <Arduino.h>
-#include <displayui.h>
 
-#define RGB_BRIGHTNESS 1 //  Brightness setting for RGB LEDs.
 #define PH_READ_PIN 8 // GPIO 8, assigned to pH probe analog input.
 #define UP_BUTTON_PIN 41
 #define DOWN_BUTTON_PIN 38
 #define LEFT_BUTTON_PIN 35
 #define RIGHT_BUTTON_PIN 47
-#define I2C_SCL_PIN 14
-#define I2C_SDA_PIN 13
 
-DisplayUI dUI;
-char cDevState; // For future use. There is a function called SetDeviceState that should be used with this variable.
-char cSelectedMenuTab;  // UI Variable for selected menu tab on the left of the display. 0 = pH, 1 = EC, 2 = Settings.
-char cpHPumpMode;   // Current pH Up/Down pump mode. 0 = Off, 1 = Up, 2 = Down
-bool bMenuTabActive;    // UI Variable for if the current cSelectedMenuTab is actively being modified by user.
-bool bUpPressed;
-bool bDownPressed;
-bool bLeftPressed;
-bool bRightPressed;
-bool bpHTimerStarted = false; // The millis() start of the pH timer. After timer has expired, this should become false and bTimerEnded true.
-bool bpHTimerEnded = false;
-bool bpHOverRange;
-bool bpHUnderRange;
-bool bWaitForMix;   // Set true when we are waiting for mixture to become homogenous based on a timer.
+bool upPressed;
+bool downPressed;
+bool leftPressed;
+bool rightPressed;
+bool pHTimerStarted = false; // The millis() start of the pH timer. After timer has expired, this should become false and bTimerEnded true.
+bool pHTimerEnded = false;
+bool pHOverRange;
+bool pHUnderRange;
 int j;
-unsigned long lCurrentMillis = millis();
-static unsigned long lLastpHMillis;  // Last millisec trig.
-static unsigned long lLastSecond; // Last second trig.
-static unsigned long lTimerStartMillis;
+unsigned long currentMillis = millis();
+static unsigned long lastpHMillis;  // Last millisec trig.
+static unsigned long lastSecond; // Last second trig.
+static unsigned long timerStartMillis;
 float rawAnalog;
-float fpHAnalogArray[10];   // Array used for pH reading calculations.
-float fpH0Cal = 1;  // Default = 1, shouldn't really need to be changed.
-float fpH4Cal = 0.622;  // Voltage in pH 4 buffer, default = 0.7146.
-float fpH7Cal = 0.5126;  // Voltage in pH 7 buffer, default 0.5.
-float fpH10Cal = 0.2865; // Voltage in pH 10 buffer, default 0.2865.
-float fpH14Cal = 0;  // Default = 0, shouldn't really need to be changed.
-float fpHAverage;
-float fpHTarget = 0;
+float pHAnalogArray[10];   // Array used for pH reading calculations.
+float pH0Cal = 1;  // Default = 1, shouldn't really need to be changed.
+float pH4Cal = 0.622;  // Voltage in pH 4 buffer, default = 0.7146.
+float pH7Cal = 0.5126;  // Voltage in pH 7 buffer, default 0.5.
+float pH10Cal = 0.2865; // Voltage in pH 10 buffer, default 0.2865.
+float pH14Cal = 0;  // Default = 0, shouldn't really need to be changed.
+float pHAverage;
+float pHTarget = 0;
