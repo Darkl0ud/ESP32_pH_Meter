0f20aea16aefe3d42e65cba82c720a2ac2d2a040
Darkl0ud <wjbrown831989@gmail.com>
2024-04-20 17:21:46 -0400

    Refactor calculations
    Refactored average pH calculations to separate

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@ void IRAM_ATTR buttonUpPressed() {
   if (digitalRead(UP_BUTTON_PIN) == 1)  {
     upPressed = true;
   }
+   
   else  {
     upPressed = false;
   }
@@ -13,6 +14,7 @@ void IRAM_ATTR buttonDownPressed() {
   if (digitalRead(DOWN_BUTTON_PIN) == 1)  {
     downPressed = true;
   }
+
   else  {
     downPressed = false;
   }
@@ -22,113 +24,119 @@ void IRAM_ATTR buttonLeftPressed() {
   if (digitalRead(LEFT_BUTTON_PIN) == 1)  {
     leftPressed = true;
   }
+
   else  {
     leftPressed = false;
   }
 }
 
-void IRAM_ATTR buttonRightPressed() {
+void IRAM_ATTR buttonRightChanged() {
   if (digitalRead(RIGHT_BUTTON_PIN) == 1)  {
     rightPressed = true;
   }
+
   else  {
     rightPressed = false;
   }
 }
 
-
-void setup()  {
-  Serial.begin(115200);
+void setupPins(){
+  // Set pH probe
   pinMode(PH_READ_PIN, INPUT);
-  pinMode(UP_BUTTON_PIN, INPUT_PULLDOWN);
-  pinMode(DOWN_BUTTON_PIN, INPUT_PULLDOWN);
-  pinMode(LEFT_BUTTON_PIN, INPUT_PULLDOWN);
-  pinMode(RIGHT_BUTTON_PIN, INPUT_PULLDOWN);
-  attachInterrupt(UP_BUTTON_PIN, buttonUpPressed, CHANGE);
-  attachInterrupt(DOWN_BUTTON_PIN, buttonDownPressed, CHANGE);
-  attachInterrupt(LEFT_BUTTON_PIN, buttonLeftPressed, CHANGE);
-  attachInterrupt(RIGHT_BUTTON_PIN, buttonRightPressed, CHANGE);
+  
+  // Set arrow keys
+  for (int& pin : arrowPins) {
+    pinMode(pin, INPUT_PULLDOWN);
+  }
+}
+
+float calculateCalibratedpH(float input, float pHRangeHigh, float pHRangeLow, float pHHighCal, float pHLowCal){
+
+  float slope = (pHRangeHigh - pHRangeLow) / (pHHighCal - pHLowCal);
+  float intercept = pHRangeHigh - slope * pHHighCal; 
+  return slope * rawAnalog + intercept;
 }
 
 void calculateAveragepH() {
-  if (currentMillis - lastpHMillis >= 100)  { // Once 100ms have passed...
-  lastpHMillis = currentMillis;  // Save the current time.
-  rawAnalog = analogRead(PH_READ_PIN) / 4095.0; // Read from pH probe. Gives an analog 0-1 value.
-  float b;
-  float pH;
-  if (rawAnalog >= pH14Cal && rawAnalog <= pH10Cal) {  // pH 14 - 10
-    b = 14 - ((14.0 - 10.0) / (pH14Cal - pH10Cal)) * pH14Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-    pH = ((14.0 - 10.0) / (pH14Cal - pH10Cal)) * rawAnalog + b;
-  }
-  
-  if (rawAnalog >= pH10Cal && rawAnalog <= pH7Cal)  {  // pH 10 - 7
-    b = 10 - ((10.0 - 7.0) / (pH10Cal - pH7Cal)) * pH10Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-    pH = ((10.0 - 7.0) / (pH10Cal - pH7Cal)) * rawAnalog + b;
-  }
-  if (rawAnalog >= pH7Cal && rawAnalog <= pH4Cal) {  // pH 7 - 4
-    b = 7 - ((7.0 - 4.0) / (pH7Cal - pH4Cal)) * pH7Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-    pH = ((7.0 - 4.0) / (pH7Cal - pH4Cal)) * rawAnalog + b;
-  }
-  if (rawAnalog >= pH4Cal && rawAnalog <= pH0Cal) {  // pH 4 - 0
-    b = 4 - ((4.0 - 0.0) / (pH4Cal - pH0Cal)) * pH4Cal;  // Calculate Y Intercept based off of pH Calibration slope.
-    pH = ((4.0 - 0.0) / (pH4Cal - pH0Cal)) * rawAnalog + b;
-  }
-  pHAnalogArray[j] = pH;
-  j++;
+  // Once 100ms have passed...
+  if (currentMillis - lastpHMillis >= 100)  { 
+    // Save the current time.
+    lastpHMillis = currentMillis;  
+    // Read from pH probe. Converts to a float 0-1 value.
+    rawAnalog = analogRead(PH_READ_PIN) / 4095.0; 
+
+    if (rawAnalog >= pH14Cal && rawAnalog <= pH10Cal) {
+      pH = calculateCalibratedpH(rawAnalog, 14, 10, pH14Cal, pH10Cal);
+    }
+
+    if (rawAnalog >= pH10Cal && rawAnalog <= pH7Cal)  {
+      pH = calculateCalibratedpH(rawAnalog, 10, 7, pH10Cal, pH7Cal);
+    }
+
+    if (rawAnalog >= pH7Cal && rawAnalog <= pH4Cal) {
+      pH = calculateCalibratedpH(rawAnalog, 7, 4, pH7Cal, pH4Cal);
+    }
+
+    if (rawAnalog >= pH4Cal && rawAnalog <= pH0Cal) {  
+      pH = calculateCalibratedpH(rawAnalog, 4, 0, pH4Cal, pH0Cal);
+    }
+
+    pHAnalogArray[j] = pH;  
+    j++;
   }
    
-  if (currentMillis - lastSecond >= 1000) {  // Once 1000ms have passed...
-    lastSecond = currentMillis;  // Save the current time.
-    j = 0; // Resets j to 0.
-    char maxIndex = 0; // Resets maxIndex to 0.
-    char minIndex = 0; // Resets minIndex to 0.
+  // Once 1000ms have passed...
+  if (currentMillis - lastSecond >= 1000) {  
+    lastSecond = currentMillis;
+    j = 0;
+    char maxIndex = 0;
+    char minIndex = 0;
 
-    for (int i = 0; i < 10; i++) {  // Check each reading that we put in the array if our max or min number is higher or lower.
+    // Check each reading that we put in the array if our max or min number is higher or lower.
+    for (int i = 0; i < 10; i++) {  
       if (pHAnalogArray[i] > pHAnalogArray[maxIndex]) {
         maxIndex = i;
       }
+
       if (pHAnalogArray[i] < pHAnalogArray[minIndex]) {
         minIndex = i;
       }
     }
 
-    pHAnalogArray[maxIndex] = 0; // Set the highest value in the pH array to 0 so it is not counted in average.
-    pHAnalogArray[minIndex] = 0; // Set the lowest value in the pH array to 0 so it is not counted in average.
-    float Sum = 0;  // Initializes sum variable to 0 for use with determining average pH over the given 10 readings.
-    unsigned char countsOverZero = 0;  // Initializes count variable to 0, which holds how many pH readings above 0 we have. Using Char to save memory.
+    // Set the highest value in the pH array to 0 so it is not counted in average.
+    pHAnalogArray[maxIndex] = 0; 
+    // Set the lowest value in the pH array to 0 so it is not counted in average.
+    pHAnalogArray[minIndex] = 0; 
 
-    for (int i = 0; i < 10; i++) {  // Check each value in pH array. If not 0, add it to sum and increase count.
+    // For use with determining average pH over the given 10 readings.
+    float Sum = 0;  
+    // Holds how many pH readings above 0 we have.
+    unsigned char countsOverZero = 0;  
+    
+    // Check each value in pH array. If not 0, add it to sum and increase count.
+    for (int i = 0; i < 10; i++) {  
       if (pHAnalogArray[i] != 0) {
         Sum += pHAnalogArray[i];
         countsOverZero++;
       }
     }
-    pHAverage = Sum/countsOverZero; // Calculate average of pH readings over 1 second
+
+    // Calculate average of pH readings over 1 second
+    pHAverage = Sum/countsOverZero; 
     Serial.println(pHAverage);
   }
+}
+ 
+ void setup()  {
+  Serial.begin(115200);
+  setupPins();
+  attachInterrupt(UP_BUTTON_PIN, buttonUpPressed, CHANGE);
+  attachInterrupt(DOWN_BUTTON_PIN, buttonDownPressed, CHANGE);
+  attachInterrupt(LEFT_BUTTON_PIN, buttonLeftPressed, CHANGE);
+  attachInterrupt(RIGHT_BUTTON_PIN, buttonRightChanged, CHANGE);
 }
 
 void loop() {
   currentMillis = millis();
   calculateAveragepH();
-  
-  if (pHTarget != 0) // If we have a pH Target set....
-  {
-    
-    if (pHAverage <= (pHTarget - 0.2))  //If pH drifts from Target pH - 0.2...
-    {
-      pHUnderRange = true;
-      pHOverRange = false;
-    }
-    else if (pHAverage >= (pHTarget + 0.2))  //If pH drifts from Target pH + 0.2...
-    {
-      pHUnderRange = false;
-      pHOverRange = true;
-    }
-    else  // If pH is within range...
-    {
-      pHUnderRange = false;
-      pHOverRange = false;
-    }
-  }
 }
--- src/main.h
+++ src/main.h
@@ -1,33 +1,32 @@
-/* Made by Darkloud using DIY-MORE PH-4502C pH controller board and used specifically
- on the ESP32-S3 DevKitC */
-
 #include <Arduino.h>
 
-#define PH_READ_PIN 8 // GPIO 8, assigned to pH probe analog input.
+// Set to the corresponding pin on your MC for the pH sensor
+#define PH_READ_PIN 8
+
+// Set to the corresponding pins on your MC for the buttons, set into an array
 #define UP_BUTTON_PIN 41
 #define DOWN_BUTTON_PIN 38
 #define LEFT_BUTTON_PIN 35
 #define RIGHT_BUTTON_PIN 47
+static int arrowPins[4] = {UP_BUTTON_PIN, DOWN_BUTTON_PIN, LEFT_BUTTON_PIN, RIGHT_BUTTON_PIN};
 
-bool upPressed;
-bool downPressed;
-bool leftPressed;
-bool rightPressed;
-bool pHTimerStarted = false; // The millis() start of the pH timer. After timer has expired, this should become false and bTimerEnded true.
-bool pHTimerEnded = false;
-bool pHOverRange;
-bool pHUnderRange;
-int j;
+// Bools store if we pressed the button when its corresponding IRAM function is called
+bool upPressed = false;
+bool downPressed= false;
+bool leftPressed= false;
+bool rightPressed= false;
+
+int j = 0;
 unsigned long currentMillis = millis();
-static unsigned long lastpHMillis;  // Last millisec trig.
-static unsigned long lastSecond; // Last second trig.
-static unsigned long timerStartMillis;
-float rawAnalog;
+static unsigned long lastpHMillis = 0;
+static unsigned long lastSecond = 0;
+static unsigned long timerStartMillis = 0;
+float rawAnalog = 0;
+float pH = 0;
 float pHAnalogArray[10];   // Array used for pH reading calculations.
 float pH0Cal = 1;  // Default = 1, shouldn't really need to be changed.
 float pH4Cal = 0.622;  // Voltage in pH 4 buffer, default = 0.7146.
 float pH7Cal = 0.5126;  // Voltage in pH 7 buffer, default 0.5.
 float pH10Cal = 0.2865; // Voltage in pH 10 buffer, default 0.2865.
 float pH14Cal = 0;  // Default = 0, shouldn't really need to be changed.
-float pHAverage;
-float pHTarget = 0;
+float pHAverage = 0;
